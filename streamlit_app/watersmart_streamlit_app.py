95b9c80da034fec1791255f089b6619d20f1c000
ankshah131 <61028925+ankshah131@users.noreply.github.com>
2025-04-03 13:20:45 -0500

    Update watersmart_streamlit_app.py

--- streamlit_app/watersmart_streamlit_app.py
+++ streamlit_app/watersmart_streamlit_app.py
@@ -213,7 +213,7 @@ with tab1:
 
         "Average precipitation": {
             'min': 0,
-            'max': 500,
+            'max': 750,
             'palette': ["#f7fbff", "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", "#08306b"]
         },
         "Average potential evapotranspiration": {
